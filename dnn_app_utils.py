5c6f811d2e905a17626b493edaf1af74b46257bc
Buddha Shrestha <shrestbuddha@gmail.com>
2018-06-26 21:12:13 -0500

    testing

--- dnn_app_utils.py
+++ dnn_app_utils.py
@@ -1,45 +1,6 @@
 import numpy as np
+from activations import *
 # import matplotlib.pyplot as plt
-import h5py
-
-
-def sigmoid(Z):
-    """
-    Implements the sigmoid activation in numpy
-
-    Arguments:
-    Z -- numpy array of any shape
-
-    Returns:
-    A -- output of sigmoid(z), same shape as Z
-    cache -- returns Z as well, useful during backpropagation
-    """
-
-    A = 1 / (1 + np.exp(-Z))
-    cache = Z
-
-    return A, cache
-
-
-def relu(Z):
-    """
-    Implement the RELU function.
-
-    Arguments:
-    Z -- Output of the linear layer, of any shape
-
-    Returns:
-    A -- Post-activation parameter, of the same shape as Z
-    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
-    """
-
-    A = np.maximum(0, Z)
-
-    assert (A.shape == Z.shape)
-
-    cache = Z
-    return A, cache
-
 
 def relu_backward(dA, cache):
     """
@@ -87,22 +48,6 @@ def sigmoid_backward(dA, cache):
 
     return dZ
 
-
-def load_data():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
-
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
-
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
 def initialize_parameters(n_x, n_h, n_y):
@@ -164,42 +109,15 @@ def initialize_parameters_deep(layer_dims):
 
 
 def linear_forward(A, W, b):
-    """
-    Implement the linear part of a layer's forward propagation.
-
-    Arguments:
-    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
-    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
-    b -- bias vector, numpy array of shape (size of the current layer, 1)
-
-    Returns:
-    Z -- the input of the activation function, also called pre-activation parameter
-    cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
-    """
 
     Z = W.dot(A) + b
-    # print("Z.shape : ",Z.shape)
-    assert (Z.shape == (W.shape[0], A.shape[1]))
+
     cache = (A, W, b)
 
     return Z, cache
 
 
 def linear_activation_forward(A_prev, W, b, activation):
-    """
-    Implement the forward propagation for the LINEAR->ACTIVATION layer
-
-    Arguments:
-    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
-    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
-    b -- bias vector, numpy array of shape (size of the current layer, 1)
-    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-
-    Returns:
-    A -- the output of the activation function, also called the post-activation value
-    cache -- a python dictionary containing "linear_cache" and "activation_cache";
-             stored for computing the backward pass efficiently
-    """
 
     if activation == "sigmoid":
         # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
@@ -215,26 +133,12 @@ def linear_activation_forward(A_prev, W, b, activation):
         # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
         A, activation_cache = softmax(A_prev)
 
-    assert (A.shape == (W.shape[0], A_prev.shape[1]))
     cache = (linear_cache, activation_cache)
 
     return A, cache
 
 
 def L_model_forward(X, parameters):
-    """
-    Implement forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
-
-    Arguments:
-    X -- data, numpy array of shape (input size, number of examples)
-    parameters -- output of initialize_parameters_deep()
-
-    Returns:
-    AL -- last post-activation value
-    caches -- list of caches containing:
-                every cache of linear_relu_forward() (there are L-1 of them, indexed from 0 to L-2)
-                the cache of linear_sigmoid_forward() (there is one, indexed L-1)
-    """
 
     caches = []
     A = X
@@ -251,12 +155,11 @@ def L_model_forward(X, parameters):
     AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation="relu")
     caches.append(cache)
 
-    # assert(AL.shape == (1,X.shape[1]))
-
     return AL, caches
 
 
 def cross_entropy_cost(AL, Y):
+
     AL_log = -np.log(AL)
 
     each_element = np.multiply(AL_log, Y)
